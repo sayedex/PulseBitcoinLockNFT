0c7e587c9f25ab4fe01a72f016608a229dbfb295
ame <sayedadmod@gmail.com>
2023-05-11 12:55:25 +0600

    fixed

--- components/Header/connect.tsx
+++ components/Header/connect.tsx
@@ -7,7 +7,7 @@ export const ExampleButton = () => {
         return (
           <button className="px-6 py-2  rounded-2xl text-sm
           font-semibold bg-black dark:bg-black  text-white whitespace-nowrap" onClick={show} >
-            {isConnected ? address?.slice(1,5)+"..."+ address?.slice(-3) : "Connect"}
+            {isConnected ? address?.slice(0,5)+"..."+ address?.slice(-3) : "Connect"}
           </button>
           
         );
